4656e3ea49e4f8a866a3ae1595379a2bf097c041
Christopher D. Long <galizur@gmail.com>
2015-06-18 08:55:23 -0400

    Women's World Cup 2015 knockout.

---
+++ fifa/wwc2015/generate_tournament.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#cp rounds_2015.csv /tmp/rounds.csv
+cp rounds_updated.csv /tmp/rounds.csv
+psql soccer -f load_rounds.sql
+rm /tmp/rounds.csv
+
+psql soccer -f update_round.sql
+
+rpl "round_id=1" "round_id=2" update_round.sql
+psql soccer -f update_round.sql
+
+rpl "round_id=2" "round_id=3" update_round.sql
+psql soccer -f update_round.sql
+
+rpl "round_id=3" "round_id=4" update_round.sql
+psql soccer -f update_round.sql
+
+rpl "round_id=4" "round_id=1" update_round.sql
+
+psql soccer -f round_p.sql > round_p.txt
+cp /tmp/round_p.csv .
+
+psql soccer -f champion_p.sql > champion_p.txt
+cp /tmp/champion_p.csv .
