7c1293d30382e63d0eed37e8dcd83ae5a7c1bad0
Christopher D. Long <galizur@gmail.com>
2015-07-15 23:15:21 -0400

    Gold Cup championship and round probabilities.

--- fifa/gc2015/champion_p.sql
+++ fifa/gc2015/champion_p.sql
@@ -5,7 +5,7 @@ r.team_name,p::numeric(4,3)
 from fifa.men_rounds r
 join fifa.teams t
   on (t.team_id,t.gender_id)=(r.team_id,'men')
-where round_id=3
+where round_id=4
 order by p desc;
 
 copy
@@ -15,7 +15,7 @@ r.team_name,p::numeric(4,3)
 from fifa.men_rounds r
 join fifa.teams t
   on (t.team_id,t.gender_id)=(r.team_id,'men')
-where round_id=3
+where round_id=4
 order by p desc
 ) to '/tmp/champion_p.csv' csv header;
 
