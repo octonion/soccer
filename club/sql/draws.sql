9ac440049029371ec01131d7d05428681c947831
Christopher D. Long <galizur@gmail.com>
2016-02-03 18:22:26 -0500

    Add dir for clean scripts.

--- club/sql/draws.sql
+++ club/sql/draws.sql
@@ -1,6 +1,5 @@
 select
-g.club_name as team,
-count(*) as g,
+year,
 sum(
 case when g.club_id=g.home_team_id
      and (home_goals > away_goals) then 1
@@ -19,37 +18,26 @@ case when club_id=home_team_id
      and (home_goals > away_goals) then 1
 else 0
 end) as l,
-sum(
-case when g.club_id=g.home_team_id then home_goals
-     else away_goals
-end) as gf,
-sum(
-case when g.club_id=g.home_team_id then away_goals
-     else home_goals
-end) as ga,
-sum(
-case when g.club_id=g.home_team_id then home_goals-away_goals
-     else away_goals-home_goals
-end) as gd,
 
+--sum(
+--case not(home_goals=away_goals) then 1
+--else 0
+--end) as w,
 sum(
-case when g.club_id=g.home_team_id
-       and (home_goals > away_goals) then 3
-     when club_id=away_team_id
-       and (home_goals < away_goals) then 3
-     when (home_goals=away_goals) then 1
+case when (home_goals=away_goals) then 1
 else 0
-end) as pts
+end)::float/count(*)
 
 from club.games g
 where
     not(g.date='LIVE')
 and g.league_key = 'barclays+premier+league'
 and g.competition='Prem'
-and g.year=2015
+--and g.year=2015
 and g.date::date <= current_date
 and g.home_goals is not null
 and g.away_goals is not null
-group by g.club_name
-order by pts desc,gd desc,gf desc;
-
+--and g.year >= 2010
+group by g.year
+order by g.year;
+--group by g.club_name;
