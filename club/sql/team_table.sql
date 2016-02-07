d50e1ae6f954750de9ec288ea5d6fb6f857119d8
Christopher D. Long <galizur@gmail.com>
2016-02-06 23:37:09 -0500

    Premier League team table.

--- club/sql/team_table.sql
+++ club/sql/team_table.sql
@@ -1,16 +1,26 @@
 
 select
-team,avg(pts)::numeric(3,1) as avg,
-min(pts) as min,
-max(pts) as max from club.sims
+team,
+avg(rank)::numeric(3,1) as rnk_avg,
+min(rank) as rnk_min,
+max(rank) as rnk_max,
+avg(pts)::numeric(3,1) as pts_avg,
+min(pts) as pts_min,
+max(pts) as pts_max
+from club.sims
 group by team
 order by team;
 
 copy (
 select
-team,avg(pts)::numeric(3,1) as avg,
-min(pts) as min,
-max(pts) as max from club.sims
+team,
+avg(rank)::numeric(3,1) as rnk_avg,
+min(rank) as rnk_min,
+max(rank) as rnk_max,
+avg(pts)::numeric(3,1) as pts_avg,
+min(pts) as pts_min,
+max(pts) as pts_max
+from club.sims
 group by team
 order by team
 ) to '/tmp/team_table.cvs' csv header;
