f13a56fb0b07eb2416383cc70675cb6f1917603b
Christopher D. Long <galizur@gmail.com>
2016-06-09 20:25:12 -0400

    Euro 2016 matchups.

---
+++ fifa/euro2016/load_rounds_eot.sql
@@ -0,0 +1,442 @@
+begin;
+
+-- rounds
+
+drop table if exists fifa.men_rounds;
+
+create table fifa.men_rounds (
+	year				integer,
+	round_id			integer,
+	team_id				text,
+	team_name			text,
+	bracket				int[],
+	p				float,
+	primary key (year,round_id,team_id)
+);
+
+copy fifa.men_rounds from '/tmp/rounds.csv' with delimiter as ',' csv header quote as '"';
+
+-- matchup probabilities
+
+drop table if exists fifa.men_matrix_p;
+
+create table fifa.men_matrix_p (
+	year				integer,
+	field				text,
+	rules				text,
+	team_id				text,
+	opponent_id			text,
+	team_p				float,
+	opponent_p			float,
+	primary key (year,field,rules,team_id,opponent_id)
+);
+
+-- 30 minutes extra time
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'home',
+'extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor*(f.exp_factor-1),'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor*(f.exp_factor-1),'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'away',
+'extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor*(f.exp_factor-1),'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor*(f.exp_factor-1),'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'neutral',
+'extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*(f.exp_factor-1),'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'win')
++
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*(f.exp_factor-1),'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*(f.exp_factor-1),
+	exp(bf.estimate)*v.offensive*h.defensive*(f.exp_factor-1),'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+-- No extra time
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'home',
+'no extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor,'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'away',
+'no extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+	exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor,'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+insert into fifa.men_matrix_p
+(year,
+field,
+rules,
+team_id,opponent_id,
+team_p,opponent_p)
+(select
+r1.year,
+'neutral',
+'no extra time',
+r1.team_id,
+r2.team_id,
+
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+as home_p,
+
+1.0-
+(
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'win')
++
+0.5*
+skellam(exp(bf.estimate)*h.offensive*v.defensive,
+	exp(bf.estimate)*v.offensive*h.defensive,'draw')
+)
+as visitor_p
+
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on ((r2.year)=(r1.year) and not((r2.team_id)=(r1.team_id)))
+join fifa.men_schedule_factors v
+  on (v.team_id)=(r2.team_id)
+join fifa.men_schedule_factors h
+  on (h.team_id)=(r1.team_id)
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+join fifa.men_factors f
+  on (f.parameter,f.level) = ('game_length','1 OT')
+where
+  r1.year=2016
+);
+
+-- home advantage
+
+drop table if exists fifa.men_matrix_field;
+
+create table fifa.men_matrix_field (
+	year				integer,
+	round_id			integer,
+	team_id				text,
+	opponent_id			text,
+	field				text,
+	primary key (year,round_id,team_id,opponent_id)
+);
+
+insert into fifa.men_matrix_field
+(year,round_id,team_id,opponent_id,field)
+(select
+r1.year,
+gs.round_id,
+r1.team_id,
+r2.team_id,
+'neutral'
+from fifa.men_rounds r1
+join fifa.men_rounds r2
+  on (r2.year=r1.year and not(r2.team_id=r1.team_id))
+join (select generate_series(1, 4) round_id) gs
+  on TRUE
+where
+  r1.year=2016
+);
+
+-- USA
+
+update fifa.men_matrix_field
+set field='home'
+where (year,team_id)=(2016,'usa');
+
+update fifa.men_matrix_field
+set field='away'
+where (year,opponent_id)=(2016,'usa');
+
+-- Mexico
+
+update fifa.men_matrix_field
+set field='home'
+where (year,team_id)=(2016,'mex')
+and not((year,opponent_id)=(2016,'usa'));
+
+update fifa.men_matrix_field
+set field='away'
+where (year,opponent_id)=(2016,'mex')
+and not((year,team_id)=(2016,'use'));
+
+commit;
---
+++ fifa/euro2016/update_round.sql
@@ -0,0 +1,43 @@
+begin;
+
+insert into fifa.men_rounds
+(year,round_id,team_id,team_name,bracket,p)
+(
+select
+r1.year as year,
+r1.round_id+1 as round,
+r1.team_id,
+r1.team_name,
+r1.bracket,
+
+sum(
+(case when r2.team_id is null then 1.0
+      else r1.p*r2.p*mp.team_p
+ end)
+) as p
+
+from fifa.men_rounds r1
+left join fifa.men_rounds r2
+  on ((r1.year,r1.round_id,r1.bracket[r1.round_id+1])=
+      (r2.year,r2.round_id,r2.bracket[r1.round_id+1])
+       and not(r1.bracket[r1.round_id]=r2.bracket[r1.round_id]))
+left join fifa.men_matrix_field mf
+  on (mf.year,mf.round_id,mf.team_id,mf.opponent_id)=
+     (r1.year,r1.round_id,r1.team_id,r2.team_id)
+left join fifa.men_matrix_p mp
+  on (mp.year,mp.field,mp.team_id,mp.opponent_id)=
+     (r1.year,mf.field,r1.team_id,r2.team_id)
+
+where
+    r1.year=2016
+and r1.round_id=1
+and (mp.rules is null or
+     mp.rules=(
+       case when r1.round_id in (1,2,3) then 'no extra time'
+            when r1.round_id in (4) then 'extra time'
+       end)
+    )
+group by r1.year,round,r1.team_id,r1.team_name,r1.bracket
+);
+
+commit;
