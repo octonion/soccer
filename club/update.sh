ed0ae95a1220598ec1ee0dc0b295df9d3810c874
Christopher D. Long <galizur@gmail.com>
2016-11-20 01:22:35 -0500

    Add more leagues to sim script

--- club/update.sh
+++ club/update.sh
@@ -16,14 +16,37 @@ psql soccer -f keys/intraleague_keys.sql
 
 cd sim_results
 
-./sim.sh portuguese+liga
-
 ./sim.sh english+premier+league
 ./sim.sh english+league+championship
 ./sim.sh english+league+one
 ./sim.sh english+league+two
 ./sim.sh english+national+league
+
+./sim.sh australian+a-league
+
+./sim.sh dutch+eredivisie
+
 ./sim.sh french+ligue+1
+./sim.sh french+ligue+2
+
 ./sim.sh german+bundesliga
+./sim.sh german+2.+bundesliga
+
 ./sim.sh italian+serie+a
+./sim.sh italian+serie+b
+
+./sim.sh japanese+j+league
+
+./sim.sh northern+irish+premiership
+
+./sim.sh portuguese+liga
+
+./sim.sh scottish+premiership
+./sim.sh scottish+championship
+./sim.sh scottish+league+one
+./sim.sh scottish+league+two
+
 ./sim.sh spanish+primera+división
+./sim.sh spanish+segunda+division
+
+./sim.sh swiss+super+league
