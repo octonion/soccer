f441e3ffa07b4fc6033b6738753a22b0065a9533
Christopher D. Long <galizur@gmail.com>
2016-11-24 02:55:07 -0500

    Weekly predictions.

--- club/python/predict.py
+++ club/python/predict.py
@@ -22,27 +22,49 @@ select
 ts.date::date,
 'home',
 t.club_name,
-(tf.exp_factor*sft.offensive),
+(tf.exp_factor*sft.offensive*sfo.defensive),
 o.club_name,
-(of.exp_factor*sfo.offensive),
+(of.exp_factor*sfo.offensive*sft.defensive),
 
-skellam(tf.exp_factor*sft.offensive,of.exp_factor*sfo.offensive,'win') as win,
-skellam(tf.exp_factor*sft.offensive,of.exp_factor*sfo.offensive,'lose') as lose,
-skellam(tf.exp_factor*sft.offensive,of.exp_factor*sfo.offensive,'draw') as draw
+skellam(exp(i.estimate)*y.exp_factor*tf.exp_factor*tol.exp_factor*sft.offensive*odl.exp_factor*sfo.defensive,exp(i.estimate)*y.exp_factor*of.exp_factor*ool.exp_factor*sfo.offensive*tdl.exp_factor*sft.defensive,'win') as win,
+
+skellam(exp(i.estimate)*y.exp_factor*tf.exp_factor*tol.exp_factor*sft.offensive*odl.exp_factor*sfo.defensive,exp(i.estimate)*y.exp_factor*of.exp_factor*ool.exp_factor*sfo.offensive*tdl.exp_factor*sft.defensive,'lose') as lose,
+
+skellam(exp(i.estimate)*y.exp_factor*tf.exp_factor*tol.exp_factor*sft.offensive*odl.exp_factor*sfo.defensive,exp(i.estimate)*y.exp_factor*of.exp_factor*ool.exp_factor*sfo.offensive*tdl.exp_factor*sft.defensive,'draw') as draw
 
 from club.games ts
 join club.teams t
   on (t.club_id,t.year)=(ts.home_team_id,ts.year)
+join club.leagues tl
+  on (tl.league_key)=(t.league_key)
 join club._schedule_factors sft
   on (sft.team_id,sft.year)=(ts.home_team_id,ts.year)
+join club._factors tol
+  on (tol.parameter,tol.level)=('offense_league',t.league_key)
+join club._factors tdl
+  on (tdl.parameter,tdl.level)=('defense_league',t.league_key)
+
 join club.teams o
   on (o.club_id,o.year)=(ts.away_team_id,ts.year)
+join club.leagues ol
+  on (ol.league_key)=(o.league_key)
 join club._schedule_factors sfo
   on (sfo.team_id,sfo.year)=(ts.away_team_id,ts.year)
+join club._factors ool
+  on (ool.parameter,ool.level)=('offense_league',o.league_key)
+join club._factors odl
+  on (odl.parameter,odl.level)=('defense_league',o.league_key)
+
 join club._factors tf on
   tf.level='offense_home'
 join club._factors of on
   of.level='defense_home'
+join club._factors y on
+  y.level='2016'
+
+join club._basic_factors i on
+  i.factor='(Intercept)'
+
 where not(ts.date='LIVE')
 and ts.date::date between current_date and current_date+6
 and ts.club_id=ts.home_team_id
@@ -55,7 +77,7 @@ cur.execute(select)
 
 rows = cur.fetchall()
 
-csvfile = open('predict_weekly.csv', 'w', newline='')
+csvfile = open('predict2_weekly.csv', 'w', newline='')
 predict = csv.writer(csvfile)
 
 header = ["game_date","team","site","opponent",
