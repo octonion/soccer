2f05901d6ac42c9f3a6cf53cac0febf57ac38f00
Christopher D. Long <galizur@gmail.com>
2017-05-19 19:35:33 -0400

    Club soccer update.

--- club/loaders/gamecast.sql
+++ club/loaders/gamecast.sql
@@ -1,5 +1,29 @@
 begin;
 
+create temporary table gc (
+	game_id		      integer,
+	year		      integer,
+	league_key	      text,
+	home_id		      integer,
+	home_color	      text,
+	home_name	      text,
+	away_id		      integer,
+	away_color	      text,
+	away_name	      text
+);
+
+copy gc from '/tmp/gamecast.csv' csv;
+
+delete from gc
+where (game_id,league_key) in
+(
+select game_id,league_key
+from gc
+group by game_id,league_key
+having count(*)>1
+)
+and year=2017;
+
 drop table if exists club.gamecast;
 
 create table club.gamecast (
@@ -15,6 +39,14 @@ create table club.gamecast (
 	primary key (game_id, league_key)
 );
 
-copy club.gamecast from '/tmp/gamecast.csv' csv;
+insert into club.gamecast
+(
+select
+distinct
+game_id,year,league_key,
+home_id,home_color,home_name,
+away_id,away_color,away_name
+from gc
+);
 
 commit;
