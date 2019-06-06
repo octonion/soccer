9f3b4de3a0d3704ed1ebecceec0bce91711e412a
Christopher D. Long <galizur@gmail.com>
2019-06-05 20:41:37 -0400

    FIFA update.

--- fifa/scrape.sh
+++ fifa/scrape.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scrapers/competitions_year.rb women 2018
-scrapers/results.rb women 2018
+scrapers/competitions_year.rb women 2019
+scrapers/results.rb women 2019
 
-scrapers/competitions_year.rb men 2018
-scrapers/results.rb men 2018
+scrapers/competitions_year.rb men 2019
+scrapers/results.rb men 2019
