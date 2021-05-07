40916b869e040378ac917af4072ce3d97ed03a99
Christopher D. Long <galizur@gmail.com>
2021-05-07 04:19:01 -0400

    EPL - expected goals.

---
+++ epl/loaders/load_games.sql
@@ -0,0 +1,34 @@
+begin;
+
+drop table if exists epl.games;
+
+set datestyle to 'SQL, YMD';
+
+create table epl.games (
+	week			text,
+	day			text,
+	date			date,
+	time			text,
+	home_team		text,
+	home_xg			float,
+	score			text,
+	away_xg			float,
+	away_team		text,
+	attendance		integer,
+	venue			text,
+	referee			text,
+	match_report		text,
+	notes			text,
+	home_score		integer,
+	away_score		integer
+);
+
+copy epl.games from '/tmp/games.csv' with delimiter as ',' csv;
+
+alter table epl.games add column game_id serial;
+
+update epl.games
+set home_score = split_part(score,'–',1)::integer,
+    away_score = split_part(score,'–',2)::integer;
+
+commit;
---
+++ epl/sos/predictions.sql
@@ -0,0 +1,64 @@
+begin;
+
+select
+
+g.date::date as date,
+sf1.team_id as home,
+sf2.team_id as away,
+(exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)::numeric(4,1) as e_home,
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive)::numeric(4,1) as e_away,
+((exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)-
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive))::numeric(4,1) as e_d,
+optimize(exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive,exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive) as superbru
+
+from epl.games g
+join epl._schedule_factors sf1
+  on (sf1.team_id)=(g.home_team)
+join epl._schedule_factors sf2
+  on (sf2.team_id)=(g.away_team)
+
+join epl._factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join epl._factors d
+  on (d.parameter,d.level)=('field','defense_home')
+
+join epl._basic_factors i
+  on (i.factor)=('(Intercept)')
+
+where
+    g.date::date >= CURRENT_DATE
+order by date asc, home asc;
+
+copy
+(
+select
+
+g.date::date as date,
+sf1.team_id as home,
+sf2.team_id as away,
+(exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)::numeric(4,1) as e_home,
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive)::numeric(4,1) as e_away,
+((exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)-
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive))::numeric(4,1) as e_d,
+optimize(exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive,exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive) as superbru
+
+from epl.games g
+join epl._schedule_factors sf1
+  on (sf1.team_id)=(g.home_team)
+join epl._schedule_factors sf2
+  on (sf2.team_id)=(g.away_team)
+
+join epl._factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join epl._factors d
+  on (d.parameter,d.level)=('field','defense_home')
+
+join epl._basic_factors i
+  on (i.factor)=('(Intercept)')
+
+where
+    g.date::date >= CURRENT_DATE
+order by date asc, home asc
+) to '/tmp/predictions.csv' csv header;
+
+commit;
